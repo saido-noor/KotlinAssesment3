a86c6f356f34509553ace7300b64c6cf26dd9ec2
saido-noor <saidoomarnoor@gmail.com>
2022-06-16 08:37:50 +0300

    first commit

---
+++ app/src/main/java/dev/saido/assesment3application/calculatorAppActivity.kt
@@ -0,0 +1,11 @@
+package dev.saido.assesment3application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class calculatorAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator_app)
+    }
+}
